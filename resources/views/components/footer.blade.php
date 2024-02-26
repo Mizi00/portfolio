1bc03f8cff5c53585869c74793e0302ce7dce3d9
Mizi00 <toufikboukichou2@gmail.com>
2024-02-26 20:57:07 +0100

    timeline + debut du footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,5 +1,19 @@
 <footer class="{{ isset($position) ? 'footer-position' : '' }}">
-    <div class="contain">
+    <div class="contain-left">
+        <div class="contain-left-element">
+            <i class="fa-solid fa-envelope"></i>
+            <a href="">toufikboukichou2@gmail.com</a>
+        </div>
+        <div class="contain-left-element">
+            <i class="fa-solid fa-phone"></i>
+            <a href="">0610404923</a>
+        </div>
+    </div>
+    <div class="contain-center">
         <p>@Copyrigth BOUKICHOU Toufik</p>
+    </div>
+    <div class="contain-right">
+        <h1>Me Retrouver ...</h1>
+        <i class="fa-brands fa-linkedin"></i>
     </div>
 </footer>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,25 +3,47 @@
 @section('body')
 <x-navbar />
 <x-sidebar/>
-<div class="container">
-  <div class="contain-card">
 
-  @for($i=0;$i<=34;$i++)
-    <article class="card">
-      <header>
-        <h3 class="h3-title">HOME</h3>
-      </header>
-      <img src="laravel.jpg" alt="">
-      <div class="content-card">
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-          Ipsum ipsa odit accusamus distinctio dolorem nesciunt facilis perferendis fuga at, sequi,</p>
-      </div>
-      <div class="button">
-        <a href="#">Voir</a>
-      </div>
-    </article>
-    @endfor
+<!--SECTION HERO-->
+<section class="hero">
+  <div class="hero-inner">
+    <h1>Portfolio</h1>
+    <p>Toufik BOUKICHOU en BTS SIO</p>
   </div>
-</div>
+    <a href="">Voir plus</a>
+</section>
+
+<!--SECTION PARCOUR-->
+<section class="parcours">
+  <div class="parcours-title">
+    <h1>Mon Parcours</h1>
+  </div>
+  <div class="parcours-timeline">
+    <div class="parcours-timeline-contain left">
+      <div class="content">
+        <h2>Bac STMG</h2>
+        <p>Lycée Wallon, obtenu en 2020</p>
+      </div>
+    </div>
+    <div class="parcours-timeline-contain right">
+      <div class="content">
+        <h2>1er année BUT</h2>
+        <p>IUT Villeneuve d'Ascq, 2021</p>
+      </div>
+    </div>
+    <div class="parcours-timeline-contain left">
+      <div class="content">
+        <h2>BTS SIO</h2>
+        <p>Lycée Gaston Berger, obtenu en 2024</p>
+      </div>
+    </div>
+    <div class="parcours-timeline-contain right">
+      <div class="content">
+        <h2>Stage Informatique 2eme année</h2>
+        <p>Réaliser chez ValHainaut Auto</p>
+      </div>
+    </div>
+  </div>
+</section>
 <x-footer/>
 @endsection
