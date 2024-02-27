53874ed5a04370d76806575273be727fc2d0721e
Mizi00 <toufikboukichou.sio@gmail.com>
2024-02-27 14:28:53 +0100

    footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,19 +1,13 @@
 <footer class="{{ isset($position) ? 'footer-position' : '' }}">
     <div class="contain-left">
-        <h1>Me Contacter ...</h1>
-        <div class="contain-left-element">
-            <i class="fa-solid fa-envelope"></i>
-            <i class="fa-solid fa-phone"></i>
-            <i class="fa-solid fa-location-dot"></i>
-        </div>
-        
-
-    </div>
-    <div class="contain-center">
-        <p>@Copyrigth BOUKICHOU Toufik</p>
+        <span><strong>à Propos</strong></span>
+        <ul>
+            <li><a href=""><i class="fa-solid fa-envelope"></i> Mail</a></li>
+            <li><a href=""><i class="fa-solid fa-file"></i> CV</a></li>
+        </ul>
     </div>
     <div class="contain-right">
-        <h1>Me Retrouver ...</h1>
+        <span>Me retrouver ...</span>
         <i class="fa-brands fa-linkedin"></i>
     </div>
 </footer>
