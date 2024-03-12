834bbec3e08137caedd47e5350a4c020f8e33cb0
Mizi00 <toufikboukichou2@gmail.com>
2024-03-12 17:22:37 +0100

    card style and title

--- resources/views/realisation.blade.php
+++ resources/views/realisation.blade.php
@@ -3,18 +3,24 @@
 @section('body')
 <x-navbar />
 <x-sidebar />
+<div class="card-title">
+  <h1>Réalisation</h1>
+</div>
+
 <div class="contain-card">
 
   @for($i=0;$i<=10;$i++) <article class="card">
-    <img src="{{ asset('img/bureau2.jpg') }}" alt="">
+    <div class="card-img">
+      <img src="{{ asset('img/face.png') }}" alt="">
+    </div>
     <div class="content-card">
       <h1>TEST</h1>
       <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.
         Ipsum ipsa odit accusamus distinctio dolorem nesciunt facilis perferendis fuga at, sequi,</p>
+      <a href="#">
+        <div class="button">Voir plus</div>
+      </a>
     </div>
-    <a href="#">
-      <div class="button">Voir plus</div>
-    </a>
     </article>
     @endfor
 </div>
