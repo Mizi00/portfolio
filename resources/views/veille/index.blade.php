122725dc6b690c7f04b11888ca7da10da66a881c
Mizi00 <toufikboukichou2@gmail.com>
2024-03-12 18:24:34 +0100

    responsive veille card fixed

--- resources/views/veille/index.blade.php
+++ resources/views/veille/index.blade.php
@@ -10,14 +10,14 @@
     </div>
     <!--NE PAS OUBLIER PARAGRAPHE POUR LA VEILLE-->
 
-    @for($i=0; $i<10; $i++)
+    @for($i=1; $i<=11; $i++)
     <div class="veille-content">
         <div class="veille-content-img">
             <img src="{{ asset('img/img.jpg') }}" alt="">
         </div>
         <div class="veille-content-element">
             <span>12/12/2023</span>
-            <h2>Article n°1</h2>
+            <h2>Article n°{{ $i }}</h2>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, a odit velit enim ad beatae.</p>
             <div class="veille-content-element-button">
                 <a href="">Voir Plus</a>
