3f9d7f838c8eaa05a98e3e3093c905a164f21663
Mizi00 <toufikboukichou2@gmail.com>
2024-03-13 11:50:02 +0100

    add veille-container and style veille

--- resources/views/veille/index.blade.php
+++ resources/views/veille/index.blade.php
@@ -1,34 +1,38 @@
 @extends('layouts.app')
 
 @section('body')
-<x-navbar/>
-<x-sidebar/>
+<x-navbar />
+<x-sidebar />
 <div class="veille">
     <div class="veille-title">
         <h1>Veille technologique</h1>
-        <p>Retrouvez ici tous les articles<p>
+        <p>Retrouvez ici tous les articles
+        <p>
     </div>
     <!--NE PAS OUBLIER PARAGRAPHE POUR LA VEILLE-->
 
-    @for($i=1; $i<=11; $i++)
-    <div class="veille-content">
-        <div class="veille-content-img">
-            <img src="{{ asset('img/img.jpg') }}" alt="">
-        </div>
-        <div class="veille-content-element">
-            <span>12/12/2023</span>
-            <h2>Article n°{{ $i }}</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, a odit velit enim ad beatae.</p>
-            <div class="veille-content-element-button">
-                <a href="">Voir Plus</a>
+    @for($i=1; $i<=11; $i++) 
+    <div class="veille-container">
+        <div class="veille-content">
+            <div class="veille-content-img">
+                <img src="https://media.tenor.com/TnpzEhvcjdAAAAAM/jen-ai-vu-des-chose.gif" alt="">
+            </div>
+            <div class="veille-content-element">
+                <span>12/12/2023</span>
+                <h2>Article n°{{ $i }}</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, a odit velit enim ad beatae.</p>
+                <div class="veille-content-element-button">
+                    <a href="">Voir Plus</a>
+                </div>
             </div>
         </div>
     </div>
-    @endfor
+
+@endfor
 
 
 </div>
 
 
-<x-footer/>
+<x-footer />
 @endsection
