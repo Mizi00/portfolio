e4abff36fa8208867f3ba71ba6746f9356f55d74
Mizi00 <toufikboukichou2@gmail.com>
2024-04-08 22:29:18 +0200

    veille article created

--- resources/views/veille/index.blade.php
+++ resources/views/veille/index.blade.php
@@ -56,25 +56,126 @@
                 applications plus performantes, plus intuitives et plus adaptables aux besoins des utilisateurs.</p>
         </div>
 
-        <h1 class="title-article">Articles</h1>
-        @for ($i = 1; $i <= 11; $i++)
+
+        <div class="container-veille">
+            <h1 class="title-article">Articles</h1>
             <div class="veille-container">
                 <div class="veille-content">
                     <div class="veille-content-img">
-                        <a href=""><img src="{{ asset('img/image.png') }}" alt=""></a>
+                        <a href="" target="_blank"><img
+                                src="https://pic.clubic.com/v1/images/2116332/raw.webp?fit=max&width=1200&hash=e78873a2aba147eff5c887130b3c9ac1c71e3029"
+                                alt=""></a>
                     </div>
                     <div class="veille-content-element">
-                        <span>12/12/2023</span>
-                        <h2><a href="">Article n°{{ $i }}</a></h2>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, a odit velit enim ad
-                            beatae.</p>
+                        <span>11 juin 2023</span>
+                        <h2><a href="https://www.clubic.com/chatgpt/actualite-472665-on-a-demande-a-chatgpt-de-faire-une-application-mobile.html"
+                                target="_blank">On a demandé à ChatGPT de faire une application mobile</a></h2>
+                        <p>Est-ce que vous savez coder ? Que la réponse soit oui ou non, impossible de passer à côté de
+                            ChatGPT dans le domaine du développement informatique. Si vous n'êtes...</p>
                         <div class="veille-content-element-button">
-                            <a href="">Voir plus</a>
+                            <a href="" target="_blank">Voir plus</a>
                         </div>
                     </div>
                 </div>
             </div>
-        @endfor
+            <div class="veille-container">
+                <div class="veille-content">
+                    <div class="veille-content-img">
+                        <a href="https://intelligence-artificielle.developpez.com/actu/346118/-Dans-5-ans-il-n-y-aura-plus-de-programmeurs-humains-predit-le-PDG-de-Stability-AI-qui-ravive-le-debat-sur-la-possible-prise-de-pouvoir-de-l-IA-dans-la-filiere-du-developpement-informatique/"
+                            target="_blank"><img src="https://f.hellowork.com/helloworkplace/2021/11/intelligence-artificielle-et-travail-1200x675.jpg" alt=""></a>
+                    </div>
+                    <div class="veille-content-element">
+                        <span>04 juil. 2023</span>
+                        <h2><a href="https://intelligence-artificielle.developpez.com/actu/346118/-Dans-5-ans-il-n-y-aura-plus-de-programmeurs-humains-predit-le-PDG-de-Stability-AI-qui-ravive-le-debat-sur-la-possible-prise-de-pouvoir-de-l-IA-dans-la-filiere-du-developpement-informatique/"
+                                target="_blank">« Dans 5 ans, il n'y aura plus de programmeurs humains »</a></h2>
+                        <p>Le CEO de GitHub a déclaré lors d’un récent entretien que ce n’est qu’une question de temps avant
+                            que l’intelligence artificielle Copilot n’écrive 80 % du code informatique...</p>
+                        <div class="veille-content-element-button">
+                            <a href="https://intelligence-artificielle.developpez.com/actu/346118/-Dans-5-ans-il-n-y-aura-plus-de-programmeurs-humains-predit-le-PDG-de-Stability-AI-qui-ravive-le-debat-sur-la-possible-prise-de-pouvoir-de-l-IA-dans-la-filiere-du-developpement-informatique/"
+                                target="_blank">Voir plus</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="veille-container">
+                <div class="veille-content">
+                    <div class="veille-content-img">
+                        <a href="https://intelligence-artificielle.developpez.com/actu/345674/92-pourcent-des-developpeurs-utiliseraient-des-outils-d-intelligence-artificielle-selon-une-enquete-de-GitHub-aupres-des-developpeurs/" target="_blank"><img src="https://pic.clubic.com/v1/images/1927773/raw?fit=max&width=1200&hash=c48181c2c22873aad46ba1231d940baa838a3e63" alt=""></a>
+                    </div>
+                    <div class="veille-content-element">
+                        <span>20 juin 2023</span>
+                        <h2><a href="https://intelligence-artificielle.developpez.com/actu/345674/92-pourcent-des-developpeurs-utiliseraient-des-outils-d-intelligence-artificielle-selon-une-enquete-de-GitHub-aupres-des-developpeurs/" target="_blank">92 % des développeurs utiliseraient des outils d'intelligence artificielle</a></h2>
+                        <p>Une enquête menée par la plateforme de collaboration entre développeurs GitHub a révélé que 92 % des programmeurs utilisent déjà l'intelligence artificielle...</p>
+                        <div class="veille-content-element-button">
+                            <a href="https://intelligence-artificielle.developpez.com/actu/345674/92-pourcent-des-developpeurs-utiliseraient-des-outils-d-intelligence-artificielle-selon-une-enquete-de-GitHub-aupres-des-developpeurs/" target="_blank">Voir plus</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="veille-container">
+                <div class="veille-content">
+                    <div class="veille-content-img">
+                        <a href="https://www.journaldunet.com/intelligence-artificielle/1522869-ia-generative-allons-nous-vraiment-vers-la-fin-des-developpeurs/" target="_blank"><img src="https://www.challenges.fr/assets/img/2023/04/27/cover-r4x3w1200-644a6459bab9e-33356-1932203-k2-k1-4465691-jpg.jpg" alt=""></a>
+                    </div>
+                    <div class="veille-content-element">
+                        <span>06 juin 2023</span>
+                        <h2><a href="https://www.journaldunet.com/intelligence-artificielle/1522869-ia-generative-allons-nous-vraiment-vers-la-fin-des-developpeurs/" target="_blank">IA générative : allons-nous vraiment vers la fin des développeurs ?</a></h2>
+                        <p>Le domaine du développement informatique n’échappe pas aux remous générés par la récente...</p>
+                        <div class="veille-content-element-button">
+                            <a href="https://www.journaldunet.com/intelligence-artificielle/1522869-ia-generative-allons-nous-vraiment-vers-la-fin-des-developpeurs/" target="_blank">Voir plus</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="veille-container">
+                <div class="veille-content">
+                    <div class="veille-content-img">
+                        <a href="https://www.cnil.fr/fr/liso-27701-une-norme-internationale-pour-la-protection-des-donnees-personnelles" target="_blank"><img src="https://www.cnil.fr/sites/cnil/files/styles/contenu_generique_visuel/public/thumbnails/image/normalisation-iso.jpg?itok=bNA7FOQs" alt=""></a>
+                    </div>
+                    <div class="veille-content-element">
+                        <span>02 avr. 2023</span>
+                        <h2><a href="https://www.cnil.fr/fr/liso-27701-une-norme-internationale-pour-la-protection-des-donnees-personnelles" target="_blank">Données personnelles et IA : l’apport des normes ISO/IEC 27701 et 42001</a></h2>
+                        <p>La norme internationale ISO/IEC 27701 décrit la gouvernance et les mesures de sécurité à mettre en place pour les traitements de données...²²</p>
+                        <div class="veille-content-element-button">
+                            <a href="https://www.cnil.fr/fr/liso-27701-une-norme-internationale-pour-la-protection-des-donnees-personnelles" target="_blank">Voir plus</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="veille-container">
+                <div class="veille-content">
+                    <div class="veille-content-img">
+                        <a href="" target="_blank"><img src="{{ asset('img/image.png') }}" alt=""></a>
+                    </div>
+                    <div class="veille-content-element">
+                        <span>12/12/2023</span>
+                        <h2><a href="" target="_blank">Article</a></h2>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, a odit velit enim ad
+                            beatae.</p>
+                        <div class="veille-content-element-button">
+                            <a href="" target="_blank">Voir plus</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="veille-container">
+                <div class="veille-content">
+                    <div class="veille-content-img">
+                        <a href="" target="_blank"><img src="{{ asset('img/image.png') }}" alt=""></a>
+                    </div>
+                    <div class="veille-content-element">
+                        <span>12/12/2023</span>
+                        <h2><a href="" target="_blank">Article</a></h2>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, a odit velit enim ad
+                            beatae.</p>
+                        <div class="veille-content-element-button">
+                            <a href="" target="_blank">Voir plus</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
 
 
     </div>
