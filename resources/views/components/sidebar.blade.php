7082f307ed6a7817ec62e1ff39fbf269e0bed85f
Mizi00 <toufikboukichou2@gmail.com>
2024-10-09 21:53:52 +0200

    delete "veille" section in sidebar

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -7,7 +7,6 @@
         <li><a href="{{ route('home') }}#about-me-id">Qui suis-je ?</a></li>
         <li><a href="{{ route('realisation') }}">Réalisations</a></li>
         <li><a href="{{ route('competences') }}">Compétence</a></li>
-        <li><a href="{{ route('veille') }}">Veille</a></li>
         <li><a href="{{ route('home') }}#contact">Contact</a></li>
     </ul>
 </div>
